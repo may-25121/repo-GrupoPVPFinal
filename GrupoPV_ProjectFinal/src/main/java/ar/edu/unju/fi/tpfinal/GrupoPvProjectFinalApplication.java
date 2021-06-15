079eb509be9031d1d0e4a55d9da50927d1ad92e0
Marisel A. Yapura <35896699@fi.unju.edu.ar>
2021-06-14 21:31:05 -0300

    Task: Subiendo el proyecto.
    Responsable: Marisel A. Yapura

---
+++ GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/GrupoPvProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoPvProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoPvProjectFinalApplication.class, args);
+	}
+
+}
