3d727ed8ef2b237e689475465bc092aede963850
AndreaFlorencia <andreabejarano629@gmail.com>
2021-06-25 10:29:36 -0300

    task:trabajando en plantillas,Crud,y controller

--- GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/ProductlinesController.java
+++ GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/controller/ProductlinesController.java
@@ -1,5 +1,80 @@
 package ar.edu.unju.fi.tpfinal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.edu.unju.fi.tpfinal.model.Productlines;
+import ar.edu.unju.fi.tpfinal.service.IProductlinesService;
+
+@Controller
 public class ProductlinesController {
 
+@Autowired
+Productlines productlines;
+
+@Autowired
+@Qualifier("productlinesServiceMysql")
+private IProductlinesService productlinesService;
+
+@GetMapping("/lineaproducto/nuevo")
+public String getProductoPage(Model model) {
+	model.addAttribute("productline",productlinesService.getProductlines());
+	return "nuevalineaproducto";
 }
+
+
+@PostMapping("/lineaproducto/guardar")
+public String saveLineProductoPage(@ModelAttribute("productline")Productlines productline, Model model) {
+//public ModelAndView saveLineProducto(@ModelAttribute("productline")Productlines productline) {
+try {
+	productlinesService.saveProductlines(productline);
+} catch (Exception e) {
+	System.out.println(e.getMessage());
+}
+model.addAttribute("productlines", productlinesService.getAllProductlines());
+
+return "listarlineasproductos";
+}
+/*
+ModelAndView modelView = new ModelAndView("listarlinaesproductos");
+productlinesService.saveProductlines(productline);
+modelView.addObject("productsline",productlinesService.getAllProductlines());
+return modelView;
+}
+*/
+@GetMapping("/lineaproducto/listar")
+public String getListarLineaProductoPage(Model model) {
+	model.addAttribute("productlines", productlinesService.getAllProductlines());
+	System.out.println(" ");
+	return "listarlineasproductos";
+
+}
+@GetMapping("/lineaproducto/editar/{productLine}")
+public String getEditarPage(@PathVariable ("productLine") String productLine, Model model) {
+	this.productlines = productlinesService.getProductlinesById(productLine);
+	model.addAttribute("productline", this.productlines);
+	return "nuevalineaproducto";
+
+}
+/*
+@GetMapping("/lineaproducto/borrar/{productLinea}")
+public String getborrarPage(@PathVariable ("productLine") String productLine, Model model) {
+	Productlines productline = productlinesService.getProductlinesById(productLine);
+	model.addAttribute("productline", productline);
+	return "";
+}
+*/
+@GetMapping("/lineaproducto/borrar/confirmar/{productLine}")
+public String getConfirmarBorrarPage(@PathVariable("productLine") String productLine, Model model) {
+	productlinesService.deleteProductlinesById(productLine);
+	model.addAttribute("productlines", productlinesService.getAllProductlines());
+	return "listarlineasproductos";
+}
+}
+
--- GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/service/imp/ProductlinesServiceImp.java
+++ GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/service/imp/ProductlinesServiceImp.java
@@ -1,5 +1,49 @@
 package ar.edu.unju.fi.tpfinal.service.imp;
 
-public class ProductlinesServiceImp {
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.tpfinal.model.Productlines;
+import ar.edu.unju.fi.tpfinal.repository.IProductlinesDAO;
+import ar.edu.unju.fi.tpfinal.service.IProductlinesService;
+@Service("productlinesServiceMysql")
+public class ProductlinesServiceImp implements IProductlinesService {
+
+	@Autowired
+	IProductlinesDAO productlinesDAO;
+	
+	@Autowired
+	private Productlines productlines;
+	
+	@Override
+	public Productlines getProductlines() {
+		// TODO Auto-generated method stub
+		return this.productlines;
+	}
+
+	@Override
+	public void saveProductlines(Productlines productlines) {
+		productlinesDAO.save(productlines);
+	}
+
+	@Override
+	public Productlines getProductlinesById(String productLine) {
+		Productlines productline = productlinesDAO.findById(productLine).get();
+		return productline;
+	}
+
+	@Override
+	public List<Productlines> getAllProductlines() {
+		List<Productlines> productlines = (List<Productlines>) productlinesDAO.findAll();
+		return productlines;
+	}
+
+	@Override
+	public void deleteProductlinesById(String productLine) {
+		productlinesDAO.deleteById(productLine);
+	}
 
 }
