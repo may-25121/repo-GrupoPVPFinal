ad1a2f8604a3ab145b397d57816883ae086935f3
Marisel A. Yapura <35896699@fi.unju.edu.ar>
2021-07-07 18:46:18 -0300

    Task: nueva clase imgConfig

---
+++ GrupoPV_ProjectFinal/src/main/java/ar/edu/unju/fi/tpfinal/imgConfig.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class imgConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		registry.addResourceHandler("/resources/**").addResourceLocations("file:/C:/Productlines/resources/");
+	}
+	
+
+}
